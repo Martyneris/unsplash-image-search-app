153caa2407113c9ab5b6f3376bb439e5b0acbe26
Martyneris <martynas.siska@gmail.com>
2018-07-19 02:59:56 +0300

    update

--- src/components/Popup.js
+++ src/components/Popup.js
@@ -1,19 +1,19 @@
 import React from 'react';
 
-const Popup =({active,question,remove,toggle})=>{
-    return(
-        <div className={active? "popup active" : "popup"}>
-        <h3>{question}</h3>
-        <button
-        onClick={()=>{
-            toggle();
-            remove();
-        }}>
-        Yes
+const Popup = ({ active, question, remove, toggle }) => {
+    return (
+        <div className={active ? "popup active" : "popup"}>
+            <h3>{question}</h3>
+            <button
+                onClick={() => {
+                    toggle();
+                    remove();
+                }}>
+                Yes
         </button>
-        <button
-        onClick={toggle}
-        >No</button>
+            <button
+                onClick={toggle}
+            >No</button>
         </div>
     )
 }
